565b3b7b58442880e4b334f6149055ef4d3f2328
JB Smith <jbsmith@coherent.systems>
2017-05-18 13:28:53 -0700

    moving @prefix into get_prefix() method in order to make it runtime usable without a recompile

--- lib/tenantex/prefix.ex
+++ lib/tenantex/prefix.ex
@@ -1,8 +1,9 @@
 defmodule Tenantex.Prefix do
-  @prefix Application.get_env(:tenantex, Tenantex)[:schema_prefix] || "tenant_"
-  def strip_prefix(table_prefix), do: String.replace_prefix(table_prefix, @prefix, "")
-
-  def schema_name(tenant) when is_integer(tenant), do: @prefix <> Integer.to_string(tenant)
-  def schema_name(tenant) when is_binary(tenant), do: @prefix <> tenant
+  def strip_prefix(table_prefix), do: String.replace_prefix(table_prefix, get_prefix(), "")
+  def schema_name(tenant) when is_integer(tenant), do: get_prefix() <> Integer.to_string(tenant)
+  def schema_name(tenant) when is_binary(tenant), do: get_prefix() <> tenant
   def schema_name(nil), do: raise ArgumentError, "Tenant can not be nil"
+  defp get_prefix() do
+    Application.get_env(:tenantex, Tenantex)[:schema_prefix] || "tenant_"
+  end
 end
