e145e7e77bf4c07a932e0cf4712b7eeac103f015
Stanislav Versilov <stas.versilov@gmail.com>
2019-09-19 16:02:40 +0400

    Loosen postgrex version requirement.
    To comply with other deps requirements better.

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Tenantex.Mixfile do
     [
       {:ecto, "~> 2.1"},
       {:mariaex, "~> 0.8.0", optional: true},
-      {:postgrex, "~> 0.13.0", optional: true},
+      {:postgrex, ">= 0.13.0", optional: true},
       {:mix_test_watch, "~> 0.2", only: :dev},
     ]
   end
